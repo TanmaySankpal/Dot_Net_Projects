821435d2325a8bf880e2e3724c79a9bf57a9fc55
Tanmay Sankpal <tanmaysankpal119@gmail.com>
2023-04-12 12:16:37 +0530

    Create code to open Calculator.cs

---
+++ code to open Calculator.cs
@@ -0,0 +1,11 @@
+private void pictureBox1_Click(object sender, EventArgs e)
+{
+    // open the calculator
+    System.Diagnostics.Process.Start("calc.exe");
+}
+
+private void button1_Click(object sender, EventArgs e)
+{
+    // open the calculator
+    System.Diagnostics.Process.Start("calc.exe");
+}
