27e3dee0b23505161213a669b62a80136cb07b3b
Tanmay Sankpal <tanmaysankpal119@gmail.com>
2023-04-12 12:15:35 +0530

    Create code to open Notepad.cs

---
+++ code to open Notepad.cs
@@ -0,0 +1,11 @@
+private void pictureBox1_Click(object sender, EventArgs e)
+{
+    // open Notepad
+    System.Diagnostics.Process.Start("notepad.exe");
+}
+
+private void button1_Click(object sender, EventArgs e)
+{
+    // open Notepad
+    System.Diagnostics.Process.Start("notepad.exe");
+}
